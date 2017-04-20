c0f1745ffd091327fd06742be52aa4f770ecb7ff
Maes95 <michel.maes95@gmail.com>
2017-04-21 01:11:00 +0200

    Selenium features

--- selenium/features/step_definitions/youtube_steps.rb
+++ selenium/features/step_definitions/youtube_steps.rb
@@ -2,26 +2,28 @@ user = "";
 url = "";
 current_canal = "";
 
-When /^Voy a Youtube$/ do 
+When /^Voy a Youtube$/ do
     visit "https://youtube.com"
 end
 
-Then /^El primer video tiene como autor el usuario dado$/ do 
-     expect(find('li:nth-child(2) > div > div > div.yt-lockup-content > div.yt-lockup-byline > a')).to have_content(user)
+Then /^El primer video tiene como autor el usuario dado$/ do
+     expect(find(".yt-lockup-title > a[title='#{user}']")).to have_content(user)
 end
 
-Then /^El primer video no tiene como autor el usuario dado$/ do 
-   expect(find('li:nth-child(2) > div > div > div.yt-lockup-content > div.yt-lockup-byline > a')).not_to have_content(user)
+Then /^El primer video no tiene como autor el usuario dado$/ do
+   elem_1 =  first(".yt-lockup-title")
+   elem_2 =  find(".yt-lockup-title > a[title='#{user}']")
+   expect(elem_1).not_to eq(elem_2)
 end
 
-When /^Hago la búsqueda$/ do 
+When /^Hago la búsqueda$/ do
     within("#masthead-search") do
-    fill_in 'search_query', with: user;
+      fill_in 'search_query', with: user
     end
     click_button 'search-btn'
 end
 
-When /^Voy a video dado$/ do 
+When /^Voy a video dado$/ do
     visit url
 end
 
@@ -30,7 +32,16 @@ Given /^Url de un video \"([^\"]*)\"$/ do |entrada|
 end
 
 Then /^Tiene que durar \"([^\"]*)\"$/ do |duracion|
-    expect(find('span.ytp-time-duration')).to have_content(duracion)
+    duracion_encontrada = find('span.ytp-time-duration').text
+    if duracion_encontrada != duracion then
+      # Posible anuncio
+      minutos = Integer(duracion_encontrada.split(':')[0])
+      segundos = Integer(duracion_encontrada.split(':')[1])
+      # Esperar al anuncio
+      sleep minutos * 60 + segundos + 2
+      duracion_encontrada = find('span.ytp-time-duration').text
+    end
+    expect(duracion_encontrada).to eq(duracion)
 end
 
 Given /^El usuario \"([^\"]*)\"$/ do |usuario|
@@ -42,8 +53,9 @@ Given /^El canal (.+)$/ do |canal|
 end
 
 When /^Pincho el canal en el menu$/ do
-    find('#appbar-guide-button').click()
-    find('#guide-container').find('a[title="' + current_canal + '"]').click()
+    find('#appbar-guide-button').click unless ! find('#guide-container').nil?
+    sleep 2
+    find('#guide-container').find('a[title="' + current_canal + '"]').click
 end
 
 When /^Pincho cualquier video$/ do
@@ -57,6 +69,10 @@ Then /^Debe contener el canal como categoría$/ do
 end
 
 Then /^El primer comentario debe tener más likes que el segundo$/ do
-    #puts find('#comment-section-renderer-items section')['innerHtml']
-    #expect(find('#comment-section-renderer-items:first-child').text).to 
+    execute_script "window.scrollBy(0,10000)"
+    sleep 10
+    first_elem = first(".off").text
+    all(".off").each do |el|
+        expect(Integer(first_elem)).to be >= Integer(el.text)
+    end
 end
